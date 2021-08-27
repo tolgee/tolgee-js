10bef279008914f7973155129d5579766801e210
Jan Cizmar <cizmar@chlupac.com>
2021-08-27 14:13:40 +0200

    fix(js sdk): multiple keys select is under other el TG-207

--- packages/ui/src/common/ContextMenu/ContextMenu.tsx
+++ packages/ui/src/common/ContextMenu/ContextMenu.tsx
@@ -47,6 +47,7 @@ export const ContextMenu: FunctionComponent<ContextMenuProps> = (props) => {
     width: width !== null ? `${width + 1}px` : undefined,
     backgroundColor: 'white',
     borderRadius: '3px',
+    zIndex: 2147483000,
     boxShadow:
       '0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14), 0px 3px 14px 2px rgba(0,0,0,0.12)',
   };
