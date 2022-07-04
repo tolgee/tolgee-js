549000f7a57a74ed7fe04a271a9ac1ddd9eb811a
Jan Cizmar <cizmar@chlupac.com>
2022-07-04 11:33:53 +0200

    chore: Refactor next internal tests & disable screenshot tests

---
+++ e2e/cypress/common/nextInternalCommon.ts
@@ -0,0 +1,33 @@
+import { createApiKey, deleteScreenshots, getScreenshots } from './apiCalls';
+import { Scope } from './types';
+
+export const openUI = (translation = 'On the road') => {
+  cy.contains(translation)
+    .should('be.visible')
+    .trigger('keydown', { key: 'Alt' })
+    .trigger('mouseover')
+    .click();
+  cy.window().trigger('keyup', { key: 'Alt' });
+  cy.get('textarea').contains(translation).should('be.visible');
+  cy.wait(300);
+};
+
+export const removeScreenshots = () => {
+  getScreenshots(1, 1000007006).then((data) => {
+    const screenshotIds = data._embedded?.screenshots.map((sc) => sc.id);
+    if (screenshotIds) {
+      return deleteScreenshots(1, 1000007006, screenshotIds);
+    }
+  });
+};
+
+export const visitWithApiKey = (scopes: Scope[]) => {
+  createApiKey({ projectId: 1, scopes })
+    .then((data) => {
+      cy.visit(`http://localhost:8114/?api_key=${data.key}`);
+    })
+    .then(() =>
+      localStorage.setItem('__tolgee_preferredLanguages', '["en","de"]')
+    );
+  cy.contains('On the road').invoke('attr', '_tolgee').should('exist');
+};
---
+++ e2e/cypress/integration/next-internal/screenshots.spec.ts
@@ -0,0 +1,90 @@
+import { login } from '../../common/apiCalls';
+import { getByAriaLabel } from '../../common/selectors';
+import {
+  openUI,
+  removeScreenshots,
+  visitWithApiKey,
+} from '../../common/nextInternalCommon';
+
+context('UI Dialog', () => {
+  beforeEach(() => {
+    login().then(() => {
+      removeScreenshots();
+    });
+  });
+
+  afterEach(() => {
+    removeScreenshots();
+  });
+
+  it.skip('makes screenshot', () => {
+    visitWithApiKey([
+      'translations.view',
+      'keys.edit',
+      'translations.edit',
+      'screenshots.view',
+      'screenshots.upload',
+    ]);
+    openUI();
+    cy.get('*[aria-label="Take screenshot"]').should('be.visible').click();
+    cy.get('*[aria-label="Screenshot"]').should('be.visible');
+  });
+
+  it.skip('screenshots not editable', () => {
+    visitWithApiKey([
+      'translations.view',
+      'keys.edit',
+      'translations.edit',
+      'screenshots.view',
+    ]);
+    openUI();
+    cy.contains('There are no screenshots.').should('be.visible');
+    getByAriaLabel('Take screenshot').should('not.exist');
+  });
+
+  it.skip('screenshots editable', () => {
+    visitWithApiKey([
+      'translations.view',
+      'keys.edit',
+      'translations.edit',
+      'screenshots.view',
+      'screenshots.upload',
+    ]);
+    openUI();
+    cy.contains('There are no screenshots.').should('be.visible');
+    getByAriaLabel('Take screenshot').should('be.visible').click();
+    getByAriaLabel('Screenshot').should('be.visible').trigger('mouseover');
+    // we should be able to delete just uploaded images
+    getByAriaLabel('Delete').should('be.visible');
+
+    cy.contains('Update').click();
+
+    openUI();
+    getByAriaLabel('Screenshot').should('be.visible').trigger('mouseover');
+    // we can't delete already saved screenshots
+    getByAriaLabel('Delete').should('not.exist');
+  });
+
+  it.skip('screenshots deletable', () => {
+    visitWithApiKey([
+      'translations.view',
+      'keys.edit',
+      'translations.edit',
+      'screenshots.view',
+      'screenshots.upload',
+      'screenshots.delete',
+    ]);
+    openUI();
+    getByAriaLabel('Take screenshot').should('be.visible').click();
+    getByAriaLabel('Screenshot').should('be.visible');
+    cy.contains('Update').click();
+
+    openUI();
+    getByAriaLabel('Screenshot').trigger('mouseover');
+    getByAriaLabel('Delete').should('be.visible').click();
+    cy.contains('Update').click();
+
+    openUI();
+    getByAriaLabel('Screenshot').should('not.exist');
+  });
+});
