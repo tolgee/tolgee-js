5b6734a9f5d9ae7fe5e346d211fee0e5ba5223c2
Jan Cizmar <cizmar@chlupac.com>
2022-07-04 11:33:53 +0200

    chore: Refactor next internal tests & disable screenshot tests

--- e2e/cypress/integration/next-internal/ui.spec.ts
+++ e2e/cypress/integration/next-internal/ui.spec.ts
@@ -1,13 +1,12 @@
-import {
-  login,
-  createApiKey,
-  getScreenshots,
-  deleteScreenshots,
-} from '../../common/apiCalls';
+import { login } from '../../common/apiCalls';
 import { getByAriaLabel } from '../../common/selectors';
-import { Scope } from '../../common/types';
 import testApiKey from '../../common/testApiKey';
 import testLanguages from '../../common/testLanguages';
+import {
+  openUI,
+  removeScreenshots,
+  visitWithApiKey,
+} from '../../common/nextInternalCommon';
 
 context('UI Dialog', () => {
   beforeEach(() => {
@@ -49,27 +48,7 @@ context('UI Dialog', () => {
       'screenshots.delete',
     ]);
     openUI();
-    cy.get('textarea').contains('On the road').click();
-    cy.focused().clear().type('Hello world');
-    cy.intercept({ path: '/v2/projects/keys/**', method: 'put' }, (req) => {
-      req.reply({
-        body: {
-          id: 1000000706,
-          name: 'on-the-road-title',
-          translations: {
-            de: { id: 1000000806, text: 'Auf dem Weg', state: 'TRANSLATED' },
-            en: { id: 1000000817, text: 'Hello world', state: 'TRANSLATED' },
-            fr: { id: 1000000828, text: 'Sur la route', state: 'TRANSLATED' },
-            cs: { id: 1000000839, text: 'Na cestu', state: 'TRANSLATED' },
-          },
-          tags: [],
-          screenshots: [],
-        },
-      });
-    }).as('updateTranslation');
-    cy.contains('Update').click();
-    cy.wait('@updateTranslation');
-    cy.contains('Hello world').should('be.visible');
+    assertCanEditEnglish();
   });
 
   it('updates translation properly when languages restricted', () => {
@@ -87,6 +66,18 @@ context('UI Dialog', () => {
     ]);
     openUI();
     cy.get('textarea').contains('Auf dem Weg').should('be.disabled');
+    assertCanEditEnglish();
+  });
+
+  it('disabled when view only', () => {
+    visitWithApiKey(['translations.view', 'screenshots.view']);
+    openUI();
+    cy.contains('There are no screenshots.').should('be.visible');
+    getByAriaLabel('Take screenshot').should('not.exist');
+    cy.contains('Update').should('be.disabled');
+  });
+
+  function assertCanEditEnglish() {
     cy.get('textarea').contains('On the road').click();
     cy.focused().clear().type('Hello world');
     cy.intercept({ path: '/v2/projects/keys/**', method: 'put' }, (req) => {
@@ -108,123 +99,5 @@ context('UI Dialog', () => {
     cy.contains('Update').click();
     cy.wait('@updateTranslation');
     cy.contains('Hello world').should('be.visible');
-  });
-
-  it('make screenshot', () => {
-    visitWithApiKey([
-      'translations.view',
-      'keys.edit',
-      'translations.edit',
-      'screenshots.view',
-      'screenshots.upload',
-    ]);
-    openUI();
-    cy.get('*[aria-label="Take screenshot"]').should('be.visible').click();
-    cy.get('*[aria-label="Screenshot"]').should('be.visible');
-  });
-
-  it('disabled when view only', () => {
-    visitWithApiKey(['translations.view', 'screenshots.view']);
-    openUI();
-    cy.contains('There are no screenshots.').should('be.visible');
-    getByAriaLabel('Take screenshot').should('not.exist');
-    cy.contains('Update').should('be.disabled');
-  });
-
-  it('screenshots not visible', () => {
-    visitWithApiKey(['translations.view', 'keys.edit', 'translations.edit']);
-    openUI();
-    cy.contains('There are no screenshots.').should('not.exist');
-    getByAriaLabel('Take screenshot').should('not.exist');
-    cy.contains('Update').should('not.be.disabled');
-  });
-
-  it('screenshots not editable', () => {
-    visitWithApiKey([
-      'translations.view',
-      'keys.edit',
-      'translations.edit',
-      'screenshots.view',
-    ]);
-    openUI();
-    cy.contains('There are no screenshots.').should('be.visible');
-    getByAriaLabel('Take screenshot').should('not.exist');
-  });
-
-  it('screenshots editable', () => {
-    visitWithApiKey([
-      'translations.view',
-      'keys.edit',
-      'translations.edit',
-      'screenshots.view',
-      'screenshots.upload',
-    ]);
-    openUI();
-    cy.contains('There are no screenshots.').should('be.visible');
-    getByAriaLabel('Take screenshot').should('be.visible').click();
-    getByAriaLabel('Screenshot').should('be.visible').trigger('mouseover');
-    // we should be able to delete just uploaded images
-    getByAriaLabel('Delete').should('be.visible');
-
-    cy.contains('Update').click();
-
-    openUI();
-    getByAriaLabel('Screenshot').should('be.visible').trigger('mouseover');
-    // we can't delete already saved screenshots
-    getByAriaLabel('Delete').should('not.exist');
-  });
-
-  it('screenshots deletable', () => {
-    visitWithApiKey([
-      'translations.view',
-      'keys.edit',
-      'translations.edit',
-      'screenshots.view',
-      'screenshots.upload',
-      'screenshots.delete',
-    ]);
-    openUI();
-    getByAriaLabel('Take screenshot').should('be.visible').click();
-    getByAriaLabel('Screenshot').should('be.visible');
-    cy.contains('Update').click();
-
-    openUI();
-    getByAriaLabel('Screenshot').trigger('mouseover');
-    getByAriaLabel('Delete').should('be.visible').click();
-    cy.contains('Update').click();
-
-    openUI();
-    getByAriaLabel('Screenshot').should('not.exist');
-  });
-
-  const openUI = (translation = 'On the road') => {
-    cy.contains(translation)
-      .should('be.visible')
-      .trigger('keydown', { key: 'Alt' })
-      .trigger('mouseover')
-      .click();
-    cy.window().trigger('keyup', { key: 'Alt' });
-    cy.get('textarea').contains(translation).should('be.visible');
-    cy.wait(300);
-  };
-
-  const removeScreenshots = () => {
-    getScreenshots(1, 1000007006).then((data) => {
-      const screenshotIds = data._embedded?.screenshots.map((sc) => sc.id);
-      if (screenshotIds) {
-        return deleteScreenshots(1, 1000007006, screenshotIds);
-      }
-    });
-  };
-
-  const visitWithApiKey = (scopes: Scope[]) => {
-    createApiKey({ projectId: 1, scopes })
-      .then((data) => {
-        cy.visit(`http://localhost:8114/?api_key=${data.key}`);
-      })
-      .then(() =>
-        localStorage.setItem('__tolgee_preferredLanguages', '["en","de"]')
-      );
-    cy.contains('On the road').invoke('attr', '_tolgee').should('exist');
-  };
+  }
 });
