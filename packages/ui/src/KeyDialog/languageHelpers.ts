8e5495b2934e1a11b689c12f68d596c058ee5633
Štěpán Granát <granat.stepan@gmail.com>
2022-09-02 13:11:46 +0200

    fix: improve UI dialog behavior when many languages

---
+++ packages/ui/src/KeyDialog/languageHelpers.ts
@@ -0,0 +1,19 @@
+import { LanguageModel } from '@tolgee/core';
+
+export const putBaseLangFirst = (languages: LanguageModel[] | undefined) => {
+  const base = languages?.find((l) => l.base);
+  if (base) {
+    return [base, ...languages.filter((val) => val !== base)];
+  }
+  return languages;
+};
+
+export const putBaseLangFirstTags = (strings: Set<string>, base?: string) => {
+  if (base && strings.has(base)) {
+    return new Set([
+      base,
+      ...Array.from(strings).filter((val) => val !== base),
+    ]);
+  }
+  return strings;
+};
