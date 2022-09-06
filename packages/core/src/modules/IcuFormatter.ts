a5f6a2cea04aa6b0c09d585eaa3292844dadcc8e
Štěpán Granát <granat.stepan@gmail.com>
2022-09-06 13:09:01 +0200

    fix: don't fail on invalid locale code

--- packages/core/src/modules/IcuFormatter.ts
+++ packages/core/src/modules/IcuFormatter.ts
@@ -3,14 +3,36 @@ import { TolgeeModule } from '../types';
 export const IcuFormatter: TolgeeModule = class {
   static type = 'formatter' as const;
 
-  cache = new Map<string, IntlMessageFormat>();
+  private locales = new Map() as Map<string, string>;
+
+  isLocaleValid(locale: string) {
+    try {
+      return Boolean(Intl.NumberFormat.supportedLocalesOf(locale).length);
+    } catch {
+      return false;
+    }
+  }
+
+  getLocale(language: string) {
+    if (!this.locales.get(language)) {
+      let localeCandidate: string = String(language).replace(/[^a-zA-Z]/g, '-');
+      while (!this.isLocaleValid(localeCandidate)) {
+        localeCandidate =
+          localeCandidate.split('-').slice(0, -1).join('-') || 'en';
+      }
+      this.locales.set(language, localeCandidate);
+    }
+    return this.locales.get(language);
+  }
 
   format({ translation, language, params }) {
     const ignoreTag = !Object.values(params).find(
       (p) => typeof p === 'function'
     );
 
-    return new IntlMessageFormat(translation, language, undefined, {
+    const locale = this.getLocale(language);
+
+    return new IntlMessageFormat(translation, locale, undefined, {
       ignoreTag,
     }).format(params) as string;
   }
