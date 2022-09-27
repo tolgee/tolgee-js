e025d3f9bb35e4c15ff51f529f41797b1aad6788
Štěpán Granát <granat.stepan@gmail.com>
2022-09-27 14:53:35 +0200

    feat: svelte namespaces test

---
+++ packages/svelte/src/lib/__integration/components/Namespaces.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import {getTranslate} from '$lib/index'
+  const { isLoading, t } = getTranslate('test');
+</script>
+
+<div>
+  {#if $isLoading}
+    <div data-testid="loading">Loading...</div>
+  {/if}
+  <div data-testid="test">{$t('test')}</div>
+  <div data-testid="test_english_fallback">
+    {$t('test_english_fallback')}
+  </div>
+  <div data-testid="non_existant">
+    {$t('non_existant', 'Non existant')}
+  </div>
+  <div data-testid="ns_fallback">
+    {$t('hello_world', { ns: ['test', ''] })}
+  </div>
+  <div data-testid="ns_double_fallback">
+    {$t('test_english_fallback', { ns: ['test', ''] })}
+  </div>
+</div>
