af99eecdb43e83f47fc681526fb914931c7239b5
Štěpán Granát <granat.stepan@gmail.com>
2022-09-29 15:16:48 +0200

    feat: prepare for rc release BREAKING CHANGE: new tolgee API

--- packages/devtools-web/types/index.d.ts
+++ packages/devtools-web/types/index.d.ts
@@ -2,6 +2,7 @@ import type { Options, TolgeePlugin } from '@tolgee/core';
 import { ObserverOptions } from '../lib/types';
 
 export declare const ContextUi: () => TolgeePlugin;
+export declare const InContextProduction: () => TolgeePlugin;
 export declare const InContextTools: (options?: {
   observer?: Partial<ObserverOptions>;
   tolgee?: Partial<Options>;
