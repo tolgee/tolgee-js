853e53f0c4906a20dae62d2f62b5c11be07e1ac6
Štěpán Granát <granat.stepan@gmail.com>
2022-10-06 12:27:16 +0200

    feat: fix extension bugs

--- packages/web/src/tools/plugin.ts
+++ packages/web/src/tools/plugin.ts
@@ -1,12 +1,12 @@
 type Props = {
   message: string;
-  recievingMessage: string;
+  recievingMessage: string[];
   data?: any;
 };
 
-export function listen<T = any>(type: string, callback: (data?: T) => any) {
+export function listen<T = any>(type: string[], callback: (data?: T) => any) {
   const handler = (e: MessageEvent) => {
-    if (e.data?.type === type) {
+    if (type.includes(e.data?.type)) {
       callback(e.data?.data);
     }
   };
@@ -18,48 +18,72 @@ export function listen<T = any>(type: string, callback: (data?: T) => any) {
   };
 }
 
-function sendAndRecieve({ message, recievingMessage, data }: Props) {
-  return new Promise((resolve, reject) => {
-    const listener = listen(recievingMessage, handler);
-    window.postMessage({ type: message, data }, window.origin);
-    const timer = setTimeout(timeout, 300);
+function sendAndRecieve<T>(
+  { message, recievingMessage, data }: Props,
+  attempts = 1
+) {
+  let cancelled = false;
+  const makeAttempt = () =>
+    new Promise((resolve, reject) => {
+      const listener = listen(recievingMessage, handler);
+      window.postMessage({ type: message, data }, window.origin);
+      const timer = setTimeout(timeout, 300);
 
-    function handler(data: any) {
-      clearTimeout(timer);
-      removeEventListener();
-      resolve(data);
-    }
-    function removeEventListener() {
-      listener.unsubscribe();
-    }
-    function timeout() {
-      removeEventListener();
-      reject(new Error(`Didn't recieve ${recievingMessage} in time.`));
-    }
-  });
+      function handler(data: any) {
+        clearTimeout(timer);
+        removeEventListener();
+        resolve(data);
+      }
+      function removeEventListener() {
+        listener.unsubscribe();
+      }
+      function timeout() {
+        removeEventListener();
+        reject(new Error(`Didn't recieve ${recievingMessage} in time.`));
+      }
+    });
+
+  return {
+    cancel: () => (cancelled = true),
+    promise: new Promise<T>(async (resolve, reject) => {
+      for (let i = 0; i < 4; i++) {
+        if (cancelled) {
+          break;
+        }
+        try {
+          const result = await makeAttempt();
+          return result;
+        } catch {
+          continue;
+        }
+      }
+      if (!cancelled) {
+        throw new Error('Tolgee extension not present');
+      }
+    }),
+  };
 }
 
 export function takeScreenshot(): Promise<string> {
   return sendAndRecieve({
     message: 'TOLGEE_TAKE_SCREENSHOT',
-    recievingMessage: 'TOLGEE_SCREENSHOT_TAKEN',
-  }) as Promise<string>;
+    recievingMessage: ['TOLGEE_SCREENSHOT_TAKEN'],
+  }).promise as Promise<string>;
 }
 
 export async function detectPlugin(): Promise<boolean> {
-  // try twice
-  for (let i = 0; i < 2; i++) {
-    try {
-      await sendAndRecieve({
+  try {
+    await sendAndRecieve(
+      {
         message: 'TOLGEE_PING',
-        recievingMessage: 'TOLGEE_PONG',
-      });
-      return true;
-    } catch {
-      continue;
-    }
+        recievingMessage: ['TOLGEE_PONG'],
+      },
+      2
+    ).promise;
+    return true;
+  } catch {
+    return false;
   }
-  return false;
 }
 
 export type LibConfig = {
@@ -72,34 +96,21 @@ export type LibConfig = {
   };
 };
 
-export async function handshakeWithExtension(
-  data: LibConfig
-): Promise<boolean> {
-  // try twice
-  for (let i = 0; i < 4; i++) {
-    try {
-      await sendAndRecieve({
+export function Handshaker() {
+  let cancelLast: undefined | (() => void) = undefined;
+  async function update(data: LibConfig): Promise<boolean> {
+    const { cancel, promise } = sendAndRecieve<boolean>(
+      {
         message: 'TOLGEE_READY',
-        recievingMessage: 'TOLGEE_PLUGIN_READY',
+        recievingMessage: ['TOLGEE_PLUGIN_READY', 'TOLGEE_PLUGIN_UPDATED'],
         data,
-      });
-      return true;
-    } catch {
-      continue;
-    }
+      },
+      4
+    );
+    cancelLast = cancel;
+    return promise;
   }
-  throw new Error('Tolgee extension not present');
-}
-
-export async function updateConfig(data: LibConfig): Promise<boolean> {
-  try {
-    await sendAndRecieve({
-      message: 'TOLGEE_CONFIG_UPDATE',
-      recievingMessage: 'TOLGEE_PLUGIN_UPDATED',
-      data,
-    });
-    return true;
-  } catch {
-    throw new Error('Tolgee extension not present');
-  }
+  return {
+    update,
+  };
 }
