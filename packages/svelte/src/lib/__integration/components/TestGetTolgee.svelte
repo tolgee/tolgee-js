bc0a538888ee85594df3dc3210706f14917ac1b2
Štěpán Granát <granat.stepan@gmail.com>
2022-10-28 21:44:41 +0200

    chore: improve svelte tests

--- packages/svelte/src/lib/__integration/components/TestGetTolgee.svelte
+++ packages/svelte/src/lib/__integration/components/TestGetTolgee.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
   import { getTolgee } from "$lib/getTolgee";
+  import type { TolgeeEvent } from "@tolgee/web";
 
-
-  const tolgeeReactive = getTolgee(['language'])
-  const tolgee = getTolgee()
+  export let events: TolgeeEvent[] = []
+  const tolgee = getTolgee(events)
 </script>
 
-<div data-testid="language-reactive">{$tolgeeReactive.getLanguage()}</div>
-<div data-testid="language-non-reactive">{$tolgee.getLanguage()}</div>
+<div>
+  <div data-testid="language">{String($tolgee.getLanguage())}</div>
+  <div data-testid="pendingLanguage">{String($tolgee.getPendingLanguage())}</div>
+  <div data-testid="fetching">{String($tolgee.isFetching())}</div>
+  <div data-testid="loading">{String($tolgee.isLoading())}</div>
+  <div data-testid="initialLoad">{String($tolgee.isInitialLoading())}</div>
+  <div data-testid="running">{String($tolgee.isRunning())}</div>
+</div>
 
--- packages/svelte/src/lib/__integration/getTolgee.spec.ts
+++ packages/svelte/src/lib/__integration/getTolgee.spec.ts
@@ -1,39 +1,125 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import '@testing-library/jest-dom';
-import { render, screen, waitFor } from '@testing-library/svelte';
+import { screen, waitFor, render } from '@testing-library/svelte';
 
-import TestGetTolgee from './components/TestGetTolgee.svelte';
-import { Tolgee, type TolgeeInstance } from '@tolgee/web';
+import { Tolgee, type TolgeeEvent, type TolgeeInstance } from '@tolgee/web';
 import { SveltePlugin } from '$lib/SveltePlugin';
+import { FormatIcu } from '@tolgee/format-icu';
+import TestComponent from './components/TestGetTolgee.svelte';
+import mockTranslations from './data/mockTranslations';
+
+const API_URL = 'http://localhost';
+
+let pending = [] as (() => void)[];
+const resolvePending = () => {
+  pending.forEach((resolve) => resolve());
+  pending = [];
+};
+
+const wrapInPromise = (data: any) => () =>
+  new Promise<any>((resolve) => pending.push(() => resolve(data)));
+
+type CheckStateProps = Partial<Record<TolgeeEvent, string>>;
+
+const checkState = (props: CheckStateProps) => {
+  Object.entries(props).forEach(([event, value]) => {
+    expect(screen.queryByTestId(event)).toContainHTML(value);
+  });
+};
 
 describe('getTranslate', () => {
   let tolgee: TolgeeInstance;
-  beforeEach(async () => {
-    tolgee = Tolgee().use(SveltePlugin()).init({
-      language: 'en',
-    });
+  let runPromise: Promise<void>;
 
-    render(TestGetTolgee);
+  beforeEach(async () => {
+    tolgee = Tolgee()
+      .use(SveltePlugin())
+      .use(FormatIcu())
+      .init({
+        apiUrl: API_URL,
+        language: 'cs',
+        staticData: {
+          cs: wrapInPromise(mockTranslations.cs),
+          'cs:test': wrapInPromise(mockTranslations['cs:test']),
+          en: wrapInPromise(mockTranslations.en),
+          'en:test': wrapInPromise(mockTranslations['en:test']),
+        },
+      });
+    runPromise = tolgee.run();
   });
 
-  it('reactivity works as expected', async () => {
-    await waitFor(() => {
-      expect(screen.queryByTestId('language-reactive').innerHTML).toContain(
-        'en'
-      );
-      expect(screen.queryByTestId('language-non-reactive').innerHTML).toContain(
-        'en'
-      );
+  it('updates initialLoading', async () => {
+    render(TestComponent, {
+      props: { events: ['initialLoad'] },
     });
 
-    await tolgee.changeLanguage('de');
-
+    checkState({ initialLoad: 'true' });
+    resolvePending();
+    await runPromise;
     await waitFor(() => {
-      expect(screen.queryByTestId('language-reactive').innerHTML).toContain(
-        'de'
-      );
-      expect(screen.queryByTestId('language-non-reactive').innerHTML).toContain(
-        'en'
-      );
+      checkState({ initialLoad: 'false' });
+    });
+    tolgee.changeLanguage('en');
+    resolvePending();
+    await waitFor(() => {
+      checkState({ initialLoad: 'false' });
+    });
+  });
+
+  it('updates language', async () => {
+    render(TestComponent, {
+      props: { events: ['language'] },
+    });
+    resolvePending();
+    await runPromise;
+    checkState({ language: 'cs' });
+    tolgee.changeLanguage('en');
+    resolvePending();
+    await waitFor(() => {
+      checkState({ language: 'en' });
+    });
+  });
+
+  it('updates pending language', async () => {
+    render(TestComponent, {
+      props: { events: ['pendingLanguage'] },
+    });
+    resolvePending();
+    await runPromise;
+    checkState({ language: 'cs', pendingLanguage: 'cs' });
+    tolgee.changeLanguage('en');
+    resolvePending();
+    await waitFor(async () => {
+      checkState({ language: 'cs', pendingLanguage: 'en' });
+    });
+  });
+
+  it('updates fetching and loading', async () => {
+    render(TestComponent, {
+      props: { events: ['loading', 'fetching'] },
+    });
+
+    checkState({ loading: 'true', fetching: 'true' });
+    resolvePending();
+    await runPromise;
+    await waitFor(async () => {
+      checkState({ loading: 'false', fetching: 'false' });
+    });
+    tolgee.changeLanguage('en');
+    await waitFor(() => {
+      checkState({ loading: 'false', fetching: 'true' });
+    });
+    resolvePending();
+    await waitFor(async () => {
+      checkState({ loading: 'false', fetching: 'false' });
+    });
+    tolgee.addActiveNs('test');
+    await waitFor(() => {
+      checkState({ loading: 'true', fetching: 'true' });
+    });
+    resolvePending();
+    await waitFor(async () => {
+      checkState({ loading: 'false', fetching: 'false' });
     });
   });
 });
