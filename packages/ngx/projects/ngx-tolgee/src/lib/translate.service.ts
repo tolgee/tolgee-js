b045d794690bbb4b5d12b8135df6327f4e3811bf
Jan Cizmar <cizmar@chlupac.com>
2022-11-17 15:00:37 +0100

    fix: Angular SSR, comments refacrtoring

--- packages/ngx/projects/ngx-tolgee/src/lib/translate.service.ts
+++ packages/ngx/projects/ngx-tolgee/src/lib/translate.service.ts
@@ -1,91 +1,44 @@
-import { Inject, Injectable, OnDestroy } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Inject, Injectable, OnDestroy, NgZone } from "@angular/core";
+import { Observable } from "rxjs";
 import {
   DefaultParamType,
   EventType,
   getTranslateProps,
   ListenerEvent,
   TFnType,
-  TolgeeInstance,
-} from '@tolgee/web';
-import { TOLGEE_INSTANCE } from './tolgee-instance-token';
+  TolgeeInstance
+} from "@tolgee/web";
+import { TOLGEE_INSTANCE } from "./tolgee-instance-token";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class TranslateService implements OnDestroy {
   private runPromise: Promise<void>;
 
-  constructor(@Inject(TOLGEE_INSTANCE) private _tolgee: TolgeeInstance) {}
+  constructor(
+    @Inject(TOLGEE_INSTANCE) private _tolgee: TolgeeInstance,
+    private _ngZone: NgZone
+  ) {
+  }
 
   get tolgee() {
     return this._tolgee;
   }
 
   /**
-   * Starts Tolgee if not started and subscribes for languageChange and translationChange events
+   * Starts Tolgee if not started.
+   *
+   * The Tolgee is run outside NgZone. The triggered events, doesn't
+   * trigger the change detection, when listening directly via Tolgee.on.
+   *
+   * However, all the emitting methods (on, translate, languageAsync)
+   * in this class are emitting back in Angular's NgZone,
+   * so the change detection works fine.
    */
   public async start(): Promise<void> {
-    if (!this.runPromise) {
-      this.runPromise = this.tolgee.run();
+    if (!this.isSSR()) {
+      await this.runIfNotRunning();
     }
-    await this.runPromise;
   }
-
-  ngOnDestroy(): void {
-    this.tolgee.stop();
-  }
-
-  on<Event extends keyof EventType>(event: Event) {
-    return new Observable<ListenerEvent<EventType[Event]>>(
-      (subscriber) => {
-        const subscription = this.tolgee.on(event, (value) => {
-          subscriber.next(value as any);
-        });
-        return () => subscription.unsubscribe();
-      }
-    );
-  }
-
-  /**
-   * Returns current language
-   */
-  get language(): string {
-    return this.tolgee.getLanguage();
-  }
-
-  /**
-   * Returns an observable emitting current language.
-   * It instantly emits current language
-   */
-  get languageAsync(): Observable<string> {
-    return new Observable((subscriber) => {
-      subscriber.next(this.tolgee.getLanguage());
-      const subscription = this.on('language').subscribe((value) => {
-        subscriber.next(value.value);
-      });
-
-      return () => subscription.unsubscribe();
-    });
-  }
-
-  /**
-   * Changes the current language
-   * @param lang The new current language (e.g. en, en-US)
-   * @return Promise<void> Resolves when translations
-   * for given language are loaded
-   */
-  public changeLanguage(lang: string) {
-    return this.tolgee.changeLanguage(lang);
-  }
-
-  /**
-   * Instantly returns translated value. May return undefined or outdated value.
-   * Use only when you cannot use translate.
-   */
-  public readonly instant: TFnType = (...args) => {
-    // @ts-ignore
-    const params = getTranslateProps(...args);
-    return this.tolgee.t(params);
-  };
 
   /**
    * Returns translation asynchronously, this method always return
@@ -116,4 +69,84 @@ export class TranslateService implements OnDestroy {
       };
     });
   };
+
+  /**
+   * Instantly returns translated value. May return undefined or outdated value.
+   * Use only when you cannot use translate.
+   */
+  public readonly instant: TFnType = (...args) => {
+    // @ts-ignore
+    const params = getTranslateParams(...args);
+    return this.tolgee.t(params);
+  };
+
+  /**
+   * Returns current language
+   */
+  get language(): string {
+    return this.tolgee.getLanguage();
+  }
+
+  /**
+   * Returns an observable emitting current language.
+   * It instantly emits current language
+   */
+  get languageAsync(): Observable<string> {
+    return new Observable((subscriber) => {
+      subscriber.next(this.tolgee.getLanguage());
+      const subscription = this.on("language").subscribe((value) => {
+        subscriber.next(value.value);
+      });
+
+      return () => subscription.unsubscribe();
+    });
+  }
+
+  /**
+   * Changes the current language
+   * @param lang The new current language (e.g. en, en-US)
+   * @return Promise<void> Resolves when translations
+   * for given language are loaded
+   */
+  public changeLanguage(lang: string) {
+    return this.tolgee.changeLanguage(lang);
+  }
+
+  /**
+   * Returns observable emitting when specified event is triggered.
+   *
+   * It's a wrapper of Tolgee.on method. The emmit is triggered inside ngZone,
+   * so change detection should work properly.
+   *
+   * @param event the event to listen
+   */
+  on<Event extends keyof EventType>(event: Event) {
+    return new Observable<ListenerEvent<EventType[Event]>>(
+      (subscriber) => {
+        const subscription = this.tolgee.on(event, (value) => {
+          this._ngZone.run(() => {
+            subscriber.next(value as any);
+          });
+        });
+        return () => subscription.unsubscribe();
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.tolgee.stop();
+  }
+
+  private async runIfNotRunning() {
+    if (!this.runPromise) {
+      this.runPromise = this._ngZone.runOutsideAngular(() => {
+        return this.tolgee.run();
+      });
+    }
+    await this.runPromise;
+  }
+
+  private isSSR() {
+    return typeof window === "undefined";
+  }
 }
