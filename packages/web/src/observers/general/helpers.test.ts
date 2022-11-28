945452be5d58f66e021262fdf7f57cbaba329dcf
Štěpán Granát <granat.stepan@gmail.com>
2022-11-28 15:45:21 +0100

    chore: add more tests

--- packages/web/src/observers/general/helpers.test.ts
+++ packages/web/src/observers/general/helpers.test.ts
@@ -1,19 +1,38 @@
-import { closestElement } from './helpers';
+import { closestElement, xPathEvaluate } from './helpers';
 
-describe('closest parent function', () => {
-  it('returns itself for elements', () => {
+describe('helper functions', () => {
+  it('closest parent on elements', () => {
     const div = document.createElement('div');
     expect(closestElement(div)).toEqual(div);
 
     expect(closestElement(document.body)).toEqual(document.body);
   });
 
-  it('returns parent for text and attribute elements', () => {
+  it('closest parent on attributes and text', () => {
     const div = document.createElement('div');
     div.textContent = 'test';
     div.setAttribute('aria-label', 'test');
 
     expect(closestElement(div.childNodes[0])).toEqual(div);
     expect(closestElement(div.getAttributeNode('aria-label')!)).toEqual(div);
+  });
+
+  it('evaluateXPath is not called on node but on parent', () => {
+    const div = document.createElement('div');
+    div.textContent = 'test';
+    div.setAttribute('aria-label', 'test');
+
+    const mock = jest
+      .spyOn(document, 'evaluate')
+      // @ts-ignore
+      .mockImplementation((_: any, contextNode: Node) => {
+        expect(contextNode).toEqual(div);
+      });
+
+    xPathEvaluate('', div);
+    xPathEvaluate('', div.childNodes[0]);
+    xPathEvaluate('', div.getAttributeNode('aria-label')!);
+
+    expect(mock).toBeCalledTimes(3);
   });
 });
