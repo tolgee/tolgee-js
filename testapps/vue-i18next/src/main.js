ea88d911309e368a56ea44e9bbee4c94d799ade4
Štěpán Granát <granat.stepan@gmail.com>
2023-01-26 19:12:26 +0100

    feat: improve i18next integration

--- testapps/vue-i18next/src/main.js
+++ testapps/vue-i18next/src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue';
-import { DevTools, I18nextPlugin, Tolgee, withTolgee } from '@tolgee/i18next';
+import {
+  DevTools,
+  I18nextPlugin,
+  Tolgee,
+  BackendFetch,
+  withTolgee,
+} from '@tolgee/i18next';
 import { FormatIcu } from '@tolgee/format-icu';
 import i18next from 'i18next';
 import ICU from 'i18next-icu';
@@ -9,17 +15,12 @@ import App from './App.vue';
 
 const tolgee = Tolgee()
   .use(DevTools())
+  .use(BackendFetch())
   .use(I18nextPlugin())
   .use(FormatIcu())
   .init({
     apiUrl: process.env.VUE_APP_TOLGEE_API_URL,
     apiKey: process.env.VUE_APP_TOLGEE_API_KEY,
-    staticData: {
-      en: () => import('../i18n/en.json'),
-      de: () => import('../i18n/de.json'),
-      fr: () => import('../i18n/fr.json'),
-      cs: () => import('../i18n/cs.json'),
-    },
   });
 
 Vue.use(VueI18Next);
