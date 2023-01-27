762d61f8348391274864f44c3d2a0d144e72b18a
Štěpán Granát <granat.stepan@gmail.com>
2023-01-27 22:16:00 +0100

    feat: DevTools correctly ommitted at production

--- packages/web/rollup.config.js
+++ packages/web/rollup.config.js
@@ -1,22 +1,22 @@
 import { buildPackage } from './rollup.common';
 
 export default [
-  // buildPackage({
-  //   input: 'src/entry-development.ts',
-  //   name: 'web.development',
-  // }),
+  buildPackage({
+    input: 'src/entry-development.ts',
+    name: 'web.development',
+  }),
   buildPackage({
     input: 'src/entry-production.ts',
     name: 'web.production',
   }),
-  // buildPackage({
-  //   input: 'src/entry-universal.ts',
-  //   name: 'web.universal',
-  // }),
+  buildPackage({
+    input: 'src/entry-universal.ts',
+    name: 'web.universal',
+  }),
 
-  // // in-context tools
-  // buildPackage({
-  //   input: 'src/entry-tools.ts',
-  //   name: 'in-context-tools',
-  // }),
+  // in-context tools
+  buildPackage({
+    input: 'src/entry-tools.ts',
+    name: 'in-context-tools',
+  }),
 ];
