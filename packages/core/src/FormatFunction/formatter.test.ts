f06da0a91e8e0c2f80ce774ad59b47d110a7d4ce
Rémi Marche <35939574+ecstrema@users.noreply.github.com>
2023-03-08 15:06:40 +0100

    feat: function formatter, add basic function formatter

---
+++ packages/core/src/FormatFunction/formatter.test.ts
@@ -0,0 +1,101 @@
+import { TranslateParams } from '../types';
+import { formatter } from './formatter';
+
+
+describe('function formatter', () => {
+  // TODO!
+
+  // test('test test', () => {
+  //   expect(getText()).toEqual('test test');
+  // });
+
+  // test('simple test', () => {
+  //   matchIcu();
+  // });
+
+  // test('this is {name}', () => {
+  //   matchIcu({ name: 'Bob' });
+  // });
+
+  // test('this is {  name  }', () => {
+  //   matchIcu({ name: 'Bob' });
+  // });
+
+  // test('{ user } has { num } apples.', () => {
+  //   matchIcu({ user: 'John', num: 2 });
+  // });
+
+  // test('passing params, but no params here', () => {
+  //   matchIcu({ user: 'John', num: 2 });
+  // });
+
+  // test('{ user } has { user } apples.', () => {
+  //   matchIcu({ user: 'John', num: 2 });
+  // });
+
+  // test("ICU: '{ parameter '} format", () => {
+  //   matchIcu();
+  // });
+
+  // test("this '{ escaped }' well", () => {
+  //   matchIcu();
+  // });
+
+  // test("this is '{ weird } but valid", () => {
+  //   matchIcu();
+  // });
+
+  // test("edge case '", () => {
+  //   matchIcu();
+  // });
+
+  // test("What's {subject}?", () => {
+  //   matchIcu({ subject: 'that' });
+  // });
+
+  // test('this is also } right', () => {
+  //   matchIcu();
+  // });
+
+  // test('this is just {} wrong', () => {
+  //   expectToThrowWithIcu(ERROR_PARAM_EMPTY);
+  // });
+
+  // test('this also { } wrong', () => {
+  //   expectToThrowWithIcu(ERROR_PARAM_EMPTY);
+  // });
+
+  // test('this plain { , } wrong', () => {
+  //   expectToThrowWithIcu(ERROR_UNEXPECTED_CHAR);
+  // });
+
+  // test('this is { unexpected', () => {
+  //   expectToThrowWithIcu(ERROR_UNEXPECTED_END);
+  // });
+
+  // test('this is obviously bad { yo yo }', () => {
+  //   expectToThrowWithIcu(ERROR_UNEXPECTED_CHAR);
+  // });
+
+  // test('this is obviously bad { yo, }', () => {
+  //   expectToThrowWithIcu(ERROR_UNEXPECTED_CHAR);
+  // });
+
+  // test('good for icu { yo, number } not for me', () => {
+  //   unsupported(ERROR_UNEXPECTED_CHAR, { yo: 6 });
+  // });
+
+  // describe('supported characters in params', () => {
+  //   characterInParamSupported(Array.from('019_axAX'));
+  // });
+
+  // describe('invalid characters in params', () => {
+  //   characterInParamFailWithIcu('[+-@#$~^&*{%)(§\'"`!?:.;<>[]\\=|☺'.split(''));
+  // });
+
+  // describe('unsupported characters which are supported in ICU', () => {
+  //   characterInParamFail(Array.from('šřýíéúůĚŽÝÁÍÉůú¨ˇ'));
+  //   characterInParamFail(['汉字', '字', 'हि', 'हिन्दी']);
+  //   characterInParamFail(['😊']);
+  // });
+});
