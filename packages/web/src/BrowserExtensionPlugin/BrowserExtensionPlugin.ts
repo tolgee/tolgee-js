aa8228ada40f4a676cd70c7bdeb883846216d20a
Štěpán Granát <granat.stepan@gmail.com>
2023-04-13 12:40:58 +0200

    fix: compatibility with react-native (#3197)

--- packages/web/src/BrowserExtensionPlugin/BrowserExtensionPlugin.ts
+++ packages/web/src/BrowserExtensionPlugin/BrowserExtensionPlugin.ts
@@ -43,7 +43,7 @@ export type BrowserExtensionProps = {
 
 let BrowserExtensionPlugin: () => TolgeePlugin = () => (tolgee) => tolgee;
 
-if (typeof window !== 'undefined') {
+if (typeof window !== 'undefined' && typeof sessionStorage !== 'undefined') {
   BrowserExtensionPlugin = (): TolgeePlugin => (tolgee) => {
     const handshaker = Handshaker();
     const getConfig = () =>
