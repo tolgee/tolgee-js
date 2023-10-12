b5eab40bacf86a31fe5ad55713ffe7d448fa9e41
Štěpán Granát <granat.stepan@gmail.com>
2023-10-12 15:46:28 +0200

    chore: use react18 way of rendering

--- testapps/react-i18next/src/index.tsx
+++ testapps/react-i18next/src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { App } from './App';
 
-ReactDOM.render(
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root')!;
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
