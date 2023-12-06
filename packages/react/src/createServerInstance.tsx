0e941b38f26f68caa978460dc3cc41a3761f5680
Štěpán Granát <granat.stepan@gmail.com>
2023-12-06 11:49:16 +0100

    feat: add createServerInstance

---
+++ packages/react/src/createServerInstance.tsx
@@ -0,0 +1,42 @@
+// @ts-ignore
+import { cache } from 'react';
+import { TFnType } from '@tolgee/web';
+
+import { TBase } from './TBase';
+import { TProps, ParamsTags } from './types';
+import React from 'react';
+import { TolgeeInstance } from '@tolgee/web';
+
+export type CreateServerInstanceOptions = {
+  createTolgee: (locale: string) => Promise<TolgeeInstance>;
+  getLocale: () => string;
+};
+
+export const createServerInstance = ({
+  createTolgee,
+  getLocale,
+}: CreateServerInstanceOptions) => {
+  const getTolgeeInstance = cache(async (locale: string) => {
+    const tolgee = await createTolgee(locale);
+    await tolgee.run();
+    return tolgee;
+  });
+
+  const getTolgee = async () => {
+    const locale = getLocale();
+    const tolgee = await getTolgeeInstance(locale);
+    return tolgee;
+  };
+
+  const getTranslate = async () => {
+    const tolgee = await getTolgee();
+    return tolgee.t;
+  };
+
+  async function T(props: TProps) {
+    const t = await getTranslate();
+    return <TBase t={t as TFnType<ParamsTags>} {...props} />;
+  }
+
+  return { getTolgeeInstance, getTolgee, getTranslate, T };
+};
--- testapps/next-app/src/tolgee/server.tsx
+++ testapps/next-app/src/tolgee/server.tsx
@@ -1,39 +1,20 @@
-import { cache } from 'react';
 import { useLocale } from 'next-intl';
 
 import { TolgeeBase, ALL_LOCALES, getStaticData } from './shared';
-import { TBase, TFnType, TProps, ParamsTags } from '@tolgee/react/server';
+import { createServerInstance } from '@tolgee/react/server';
 
-export const getTolgeeInstance = cache(async (locale: string) => {
-  const tolgee = TolgeeBase().init({
-    staticData: await getStaticData(ALL_LOCALES),
-    observerOptions: {
-      fullKeyEncode: true,
-    },
-    language: locale,
-    fetch: async (input, init) => {
-      const data = await fetch(input, { ...init, next: { revalidate: 0 } });
-      return data;
-    },
-  });
-
-  await tolgee.run();
-
-  return tolgee;
+export const { getTolgee, getTranslate, T } = createServerInstance({
+  getLocale: useLocale,
+  createTolgee: async (locale) =>
+    TolgeeBase().init({
+      staticData: await getStaticData(ALL_LOCALES),
+      observerOptions: {
+        fullKeyEncode: true,
+      },
+      language: locale,
+      fetch: async (input, init) => {
+        const data = await fetch(input, { ...init, next: { revalidate: 0 } });
+        return data;
+      },
+    }),
 });
-
-export const getTolgee = async () => {
-  const locale = useLocale();
-  const tolgee = await getTolgeeInstance(locale);
-  return tolgee;
-};
-
-export const getTranslate = async () => {
-  const tolgee = await getTolgee();
-  return tolgee.t;
-};
-
-export async function T(props: TProps) {
-  const t = await getTranslate();
-  return <TBase t={t as TFnType<ParamsTags>} {...props} />;
-}
