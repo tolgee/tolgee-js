6b247b73f63de86b5041b9e7b40b9b0a6e616e13
Jan Cizmar <cizmar@chlupac.com>
2024-05-07 11:28:45 +0200

    chore: Fix jest issues

--- packages/ngx/projects/ngx-tolgee/jest.config.js
+++ packages/ngx/projects/ngx-tolgee/jest.config.js
@@ -1,23 +1,5 @@
-const { pathsToModuleNameMapper } = require('ts-jest');
-const { compilerOptions } = require('../../tsconfig.json');
-
 module.exports = {
-  preset: 'ts-jest',
-  resolver: 'jest-preset-angular/build/resolvers/ng-jest-resolver.js',
-  transformIgnorePatterns: ['node_modules/.*/node_modules/(?!@angular)'],
-  transform: {
-    '^.+\\.(ts|js|mjs|html|svg)$': 'jest-preset-angular',
-  },
-  testEnvironment: 'jsdom',
-  unmockedModulePathPatterns: ['/node_modules/*', '/ngx-tolgee/src/__mocks'],
-  modulePathIgnorePatterns: ['cypress'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/../../',
-  }),
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
 };
