8f811f6d9427eea414bdb3fc28b3797c9e6c2736
Štěpán Granát <granat.stepan@gmail.com>
2024-06-12 16:13:57 +0200

    fix: check if document is defined when creating cache in emotion

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -24,6 +24,15 @@ export const createConfig = ({ entry, rollupOptions }: Props) =>
       rollupOptions,
     },
     plugins: [
+      // this check in emotion package is not safe enough
+      replaceCodePlugin({
+        replacements: [
+          {
+            from: "typeof HTMLElement !== 'undefined'",
+            to: "(typeof document !== 'undefined' && typeof HTMLElement !== 'undefined')",
+          },
+        ],
+      }),
       react(),
       replaceCodePlugin({
         replacements: [
