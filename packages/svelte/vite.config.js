008f1fce42de9efb3359bb4141b14d3b4ffa722b
Štěpán Granát <granat.stepan@gmail.com>
2024-09-20 13:34:21 +0200

    fix: es2017 compatibility

--- packages/svelte/vite.config.js
+++ packages/svelte/vite.config.js
@@ -2,6 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
+  build: {
+    target: 'es2017',
+  },
   plugins: [sveltekit()],
 };
 
