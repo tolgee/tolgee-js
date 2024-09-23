784b55d2c1cfe61e5c21e751a64dcbfe610c3704
Štěpán Granát <granat.stepan@gmail.com>
2024-09-23 17:16:05 +0200

    chore: revert next intl messages usage

--- testapps/next-app/src/i18n/request.ts
+++ testapps/next-app/src/i18n/request.ts
@@ -1,10 +1,8 @@
 import { getRequestConfig } from 'next-intl/server';
-import { getStaticData } from '../tolgee/shared';
 
 export default getRequestConfig(async ({ locale }) => {
   return {
-    // it's important you provide all data which are needed for initial render
-    // so current locale and also fallback locales + necessary namespaces
-    messages: await getStaticData(['en', locale]),
+    // do this to make next-intl not emmit any warnings
+    messages: { locale },
   };
 });
