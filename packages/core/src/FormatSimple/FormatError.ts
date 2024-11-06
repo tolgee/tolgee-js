358707421c721a0454f6be9d2963ccf27820ba76
Štěpán Granát <granat.stepan@gmail.com>
2024-11-06 08:50:32 +0100

    fix: add hint to simple parser error message (#3399)
    Closing #3394

--- packages/core/src/FormatSimple/FormatError.ts
+++ packages/core/src/FormatSimple/FormatError.ts
@@ -12,14 +12,19 @@ export class FormatError extends Error {
   public readonly index: number;
   constructor(code: ErrorCode, index: number, text: string) {
     let error: string;
+    let hint = '';
     if (code === ERROR_PARAM_EMPTY) {
       error = 'Empty parameter';
     } else if (code === ERROR_UNEXPECTED_CHAR) {
       error = 'Unexpected character';
+      hint = 'Did you forget to use FormatIcu to render ICU message syntax?';
     } else {
       error = 'Unexpected end';
     }
-    super(`Tolgee parser: ${error} at ${index} in "${text}"`);
+    super(
+      `Tolgee parser: ${error} at ${index} in "${text}"` +
+        (hint ? '\n' + hint : '')
+    );
     this.code = code;
     this.index = index;
   }
