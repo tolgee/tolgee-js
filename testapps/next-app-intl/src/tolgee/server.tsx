76fc58842b448f01957057bddea33469e922cb17
Štěpán Granát <granat.stepan@gmail.com>
2025-01-06 17:04:04 +0100

    fix: simplify stuff

--- testapps/next-app-intl/src/tolgee/server.tsx
+++ testapps/next-app-intl/src/tolgee/server.tsx
@@ -5,14 +5,11 @@ import { TolgeeBase } from './shared';
 export const { getTolgee, getTranslate, T } = createServerInstance({
   getLocale: getLocale,
   createTolgee: async (language) => {
-    const tolgee = TolgeeBase().init({
+    return TolgeeBase().init({
       observerOptions: {
         fullKeyEncode: true,
       },
       language,
     });
-    // preload all the languages for the server instance
-    await tolgee.loadRequired();
-    return tolgee;
   },
 });
--- testapps/next-app/src/tolgee/server.tsx
+++ testapps/next-app/src/tolgee/server.tsx
@@ -5,13 +5,11 @@ import { createServerInstance } from '@tolgee/react/server';
 export const { getTolgee, getTranslate, T } = createServerInstance({
   getLocale: getLanguage,
   createTolgee: async (language) => {
-    const tolgee = TolgeeBase().init({
+    return TolgeeBase().init({
       observerOptions: {
         fullKeyEncode: true,
       },
       language,
     });
-    await tolgee.loadRequired();
-    return tolgee;
   },
 });
