6a880356285a11183a4dce3f2ccfa25db838240a
Nazarii Oliinyk <nazarii@tolgee.io>
2025-04-21 17:12:39 +0200

    Updated the headers code too looke more neat and not to interfere with other code

--- .github/scripts/update-readme.sh
+++ .github/scripts/update-readme.sh
@@ -4,7 +4,14 @@ set -euo pipefail
 project="$1"
 app_dir="testapps/$project"
 
-header="<p align=\"center\"><strong>🚨🚨🚨 This repo is just a dummy 🚨🚨🚨</strong><br>Submit issues in the <a href=\"https://github.com/tolgee/tolgee-js\" target=\"_blank\"><strong>monorepo</strong></a> or <a href=\"https://github.com/tolgee/tolgee-js/tree/main/testapps/$project\" target=\"_blank\"><strong>check the source code here</strong></a>.</p>"
+header="<p align=\"center\">
+    <strong>🚨🚨🚨 This repo is just a dummy 🚨🚨🚨</strong><br>
+    Submit issues in the 
+    <a href=\"https://github.com/tolgee/tolgee-js\" target=\"_blank\"><strong>monorepo</strong></a> 
+    or 
+    <a href=\"https://github.com/tolgee/tolgee-js/tree/main/testapps/$project\" target=\"_blank\"><strong>check the source code here</strong></a>.
+</p>
+"
 
 tmp=$(mktemp)
 echo "$header" | cat - "$app_dir/README.md" > "$tmp"
