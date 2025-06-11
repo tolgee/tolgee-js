ddbd3fdb2c02aba65bca369b8ebabee47d5521ef
Štěpán Granát <granat.stepan@gmail.com>
2025-06-11 13:00:06 +0200

    chore: add tests

--- packages/web/src/package/tools/url.test.ts
+++ packages/web/src/package/tools/url.test.ts
@@ -1,4 +1,4 @@
-import { joinUrls } from './url';
+import { createUrl, joinUrls } from './url';
 
 describe('url module', () => {
   it('joins urls without double slash', () => {
@@ -9,7 +9,13 @@ describe('url module', () => {
     expect(joinUrls('a/b/', '/c/')).toEqual('a/b/c/');
   });
 
-  it("doesn't touch the protocol", () => {
+  it("won't fail when origin invalid", () => {
     expect(joinUrls('https://test.com/', '/c/')).toEqual('https://test.com/c/');
+
+    // @ts-ignore
+    window.location = { ...window.location, origin: 'null' };
+    expect(createUrl('https://test.com/', '/c/').toString()).toEqual(
+      'https://test.com/c/'
+    );
   });
 });
