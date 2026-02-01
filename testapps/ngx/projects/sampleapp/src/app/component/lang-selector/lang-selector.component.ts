dc93bf8f562a57f5101a14cd995eb2a34a92a9ad
Vojtech Masek <vojtech@flowup.cz>
2026-02-02 00:29:29 +0100

    refactor(ngx): migrate sample app to Angular 20 with standalone components, update dependencies, and remove deprecated modules

--- testapps/ngx/projects/sampleapp/src/app/component/lang-selector/lang-selector.component.ts
+++ testapps/ngx/projects/sampleapp/src/app/component/lang-selector/lang-selector.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TranslateService } from '@tolgee/ngx';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-lang-selector',
   templateUrl: './lang-selector.component.html',
+  imports: [AsyncPipe],
 })
-export class LangSelectorComponent implements OnInit {
-  constructor(public translateService: TranslateService) {}
-
-  ngOnInit(): void {}
+export class LangSelectorComponent {
+  translateService = inject(TranslateService);
 }
