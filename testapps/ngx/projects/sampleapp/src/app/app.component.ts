bafe64ca2d4b7de9b46638efba9a2b44e7d16f0a
Vojtech Masek <vojtech@flowup.cz>
2026-02-02 00:29:29 +0100

    refactor(ngx): migrate sample app to Angular 20 with standalone components, update dependencies, and remove deprecated modules

--- testapps/ngx/projects/sampleapp/src/app/app.component.ts
+++ testapps/ngx/projects/sampleapp/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [RouterOutlet],
 })
-export class AppComponent {
-  constructor() {}
-}
+export class AppComponent {}
