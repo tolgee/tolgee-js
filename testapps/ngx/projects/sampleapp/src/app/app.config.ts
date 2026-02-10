98e7126f1980d249b532bdface17abfd3404c74d
Vojtech Masek <vojtech@flowup.cz>
2026-02-10 17:10:42 +0100

    refactor(ngx): add SSR and prerender setup

--- testapps/ngx/projects/sampleapp/src/app/app.config.ts
+++ testapps/ngx/projects/sampleapp/src/app/app.config.ts
@@ -1,43 +1,13 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from '@angular/router';
-import { routes } from './app.routes';
-import { provideTolgee } from '@tolgee/ngx';
-import { Tolgee, DevTools } from '@tolgee/ngx';
+import { mergeApplicationConfig } from '@angular/core';
+import { provideTolgee, Tolgee, DevTools } from '@tolgee/ngx';
 import { FormatIcu } from '@tolgee/format-icu';
-import { environment } from '../environments/environment';
+import { APP_BASE_CONFIG, tolgeeConfig } from './app.config.common';
 
-export const appConfig: ApplicationConfig = {
+export const appConfig = mergeApplicationConfig(APP_BASE_CONFIG, {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withEnabledBlockingInitialNavigation()),
+    // Add DevTools for browser/development
     provideTolgee(() =>
-      Tolgee()
-        .use(FormatIcu())
-        .use(DevTools())
-        .init({
-          availableLanguages: ['en', 'cs', 'fr', 'de'],
-          staticData: {
-            'en:namespaced': () =>
-              import('../i18n/namespaced/en.json').then((m) => m.default),
-            'cs:namespaced': () =>
-              import('../i18n/namespaced/cs.json').then((m) => m.default),
-            'de:namespaced': () =>
-              import('../i18n/namespaced/de.json').then((m) => m.default),
-            'fr:namespaced': () =>
-              import('../i18n/namespaced/fr.json').then((m) => m.default),
-            en: () => import('../i18n/en.json').then((m) => m.default),
-            cs: () => import('../i18n/cs.json').then((m) => m.default),
-            de: () => import('../i18n/de.json').then((m) => m.default),
-            fr: () => import('../i18n/fr.json').then((m) => m.default),
-          },
-          apiUrl: environment.tolgeeApiUrl,
-          apiKey: environment.tolgeeApiKey,
-          fallbackLanguage: 'en',
-          defaultLanguage: 'en',
-        })
+      Tolgee().use(FormatIcu()).use(DevTools()).init(tolgeeConfig)
     ),
   ],
-};
+});
